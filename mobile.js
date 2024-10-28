8898948508bdf70445a3a2c6fe20f57742e4e09f
cmd-keyu <keyumatt@gmail.com>
2024-10-28 18:03:13 +0100

    2nd commit

--- mobile.js
+++ mobile.js
@@ -1,6 +1,15 @@
-let menu = document.querySelector(".menu")
-let ul = document.querySelector("ul")
-
-menu.addEventListener("click", () => {
-    ul.style.display = "inline-block"
-}) 
+const menu = document.querySelector("#checkbox")
+const ul = document.querySelector("ul")
+const closes = document.querySelector(".close")
+const opens = document.querySelector(".open")
+menu.addEventListener("change", () =>{
+    if(menu.checked){
+        closes.style.display = "inherit";
+        opens.style.display= "none";
+        ul.style.display = "inline-flex";
+    }else{
+        closes.style.display = "none";
+        opens.style.display= "inherit";
+        ul.style.display = "none";
+    }
+});
