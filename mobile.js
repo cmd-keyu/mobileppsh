0df7ec57d9f3f672fe454cd3cd085dbc6c1debee
cmd-keyu <keyumatt@gmail.com>
2024-10-30 19:53:09 +0100

    15 mehn

--- mobile.js
+++ mobile.js
@@ -3,7 +3,6 @@ const ul = document.querySelector("ul")
 const closes = document.querySelector(".close")
 const opens = document.querySelector(".open")
 const search = document.querySelector(".search")
-const search1 = document.querySelector(".search1")
 menu.addEventListener("click", () =>{
     if(menu.checked){
         closes.style.display = "inherit";
@@ -16,4 +15,23 @@ menu.addEventListener("click", () =>{
         ul.style.display = "none";
         search.style.display = "inherit"
     }
-});
+})
+
+const menu2 = document.querySelector("#checkbox2")
+const ul2 = document.querySelector(".ul2")
+const closes2 = document.querySelector(".closes")
+const opens2 = document.querySelector(".opens")
+const search2 = document.querySelector(".search2")
+menu2.addEventListener("click", () => {
+    if(menu2.checked){
+        closes2.style.display = "inherit";
+        opens2.style.display= "none";
+        ul2.style.display = "inline-block";
+        search2.style.display = "none"
+    }else{
+        closes2.style.display = "none";
+        opens2.style.display= "inherit";
+        ul2.style.display = "none";
+        search2.style.display = "inherit"
+    }
+})
